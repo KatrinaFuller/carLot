fbd7d7dd4eedecf4437205cae96d2f46ad0f2c0d
Katrina Fuller <trinafuller@att.net>
2019-11-14 15:23:25 -0700

    added authorId to vehicleService

--- server/services/VehicleService.js
+++ server/services/VehicleService.js
@@ -7,7 +7,8 @@ const _model = new Schema({
   make: { type: String, required: true },
   model: { type: String, required: true },
   year: { type: String, required: true },
-  price: { type: Number, required: true }
+  price: { type: Number, required: true },
+  authorId: { type: ObjectId, ref: 'User', required: true }
 })
 
 export default class VehicleService {
