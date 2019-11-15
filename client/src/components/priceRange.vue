6d4eb32f66a0894cdef698ca8b3d2162485aee94
Katrina Fuller <trinafuller@att.net>
2019-11-15 14:15:06 -0700

    made priceRange component

---
+++ client/src/components/priceRange.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="priceRange"></div>
+</template>
+
+
+<script>
+export default {
+  name: "priceRange",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
